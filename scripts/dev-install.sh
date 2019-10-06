fd4dc9b862e991fb7e11840ba39a192eeb4095c7
Felix Häusler <hfelix@protonmail.ch>
2019-10-06 21:25:26 +0200

    Update SDK and runtime

--- scripts/dev-install.sh
+++ scripts/dev-install.sh
@@ -2,5 +2,5 @@
 set -e
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.freedesktop.Platform//18.08 org.freedesktop.Sdk//18.08
-flatpak install flathub org.electronjs.Electron2.BaseApp//18.08
+flatpak install flathub org.freedesktop.Platform//19.08 org.freedesktop.Sdk//19.08
+flatpak install flathub org.electronjs.Electron2.BaseApp//19.08
