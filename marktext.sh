6758fb7f984ac163fb1dc87b2d8e6470e4a1ab51
Erick555 <37542552+Erick555@users.noreply.github.com>
2020-02-27 12:22:58 +0000

    Use zypak-wrapper instead of disabling sandbox
    Also: export `TMPDIR`  to solve potential multiple instance issue.
    
    Also: using `exec` will close leftover shell process.

--- marktext.sh
+++ marktext.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# We need to disable Chromiums sandbox due flatpak#3044. This should be ok
-# because Mark Text don't use the renderer sandbox.
-/app/marktext/marktext --no-sandbox "$@"
-exit $?
+export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
+exec zypak-wrapper /app/marktext/marktext "$@"
