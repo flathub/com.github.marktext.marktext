de06a87f781b753ff8511086a7a929ebeffab838
Felix Häusler <hfelix@protonmail.ch>
2022-02-24 17:21:45 +0100

    Update to v0.17.0 (#18)

--- scripts/dev-install.sh
+++ scripts/dev-install.sh
@@ -2,5 +2,5 @@
 set -e
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.freedesktop.Platform//20.08 org.freedesktop.Sdk//20.08
-flatpak install flathub org.electronjs.Electron2.BaseApp//20.08
+flatpak install flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
+flatpak install flathub org.electronjs.Electron2.BaseApp//21.08
