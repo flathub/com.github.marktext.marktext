15bfdb1d66e9e5e64785c2bdb25e3fc82e859a7e
Brzuszek Maël <mael.brzuszek@ecl21.ec-lyon.fr>
2023-12-22 19:38:41 +0100

    Update versions in dev-install.sh

--- scripts/dev-install.sh
+++ scripts/dev-install.sh
@@ -2,5 +2,5 @@
 set -e
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
-flatpak install flathub org.electronjs.Electron2.BaseApp//21.08
+flatpak install flathub org.freedesktop.Platform//23.08 org.freedesktop.Sdk//23.08
+flatpak install flathub org.electronjs.Electron2.BaseApp//23.08
