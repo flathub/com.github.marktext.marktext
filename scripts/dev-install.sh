25acc288ed0e3b82da80d69fa22c1a47e69085a7
Benjamin Loison <benjamin_loison@users.noreply.gitea.lemnoslife.com>
2025-10-11 23:50:40 +0200

    Update org.freedesktop.Platform from 23.08 to 25.08

--- scripts/dev-install.sh
+++ scripts/dev-install.sh
@@ -2,5 +2,5 @@
 set -e
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.freedesktop.Platform//23.08 org.freedesktop.Sdk//23.08
-flatpak install flathub org.electronjs.Electron2.BaseApp//23.08
+flatpak install flathub org.freedesktop.Platform//25.08 org.freedesktop.Sdk//25.08
+flatpak install flathub org.electronjs.Electron2.BaseApp//25.08
